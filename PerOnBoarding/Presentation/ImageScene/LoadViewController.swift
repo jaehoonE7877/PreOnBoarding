e6787cb6cbf24a4be5042c13827914975be1a62e
jaehoon <sjh7877@naver.com>
2023-02-18 16:12:34 +0900

    Intial Commit

---
+++ PerOnBoarding/Presentation/ImageScene/LoadViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoadViewController.swift
+//  PerOnBoarding
+//
+//  Created by Seo Jae Hoon on 2023/02/18.
+//
+
+import UIKit
+
+final class LoadViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ PerOnBoarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PerOnBoarding
+//
+//  Created by Seo Jae Hoon on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
